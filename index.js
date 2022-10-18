2b02e71faf6995754680ea79313c96f773eaacf2
jannatul114 <jfmithila114@gmail.com>
2022-10-18 20:47:19 +0600

    product api added

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function run() {
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await userCollection.findOne(query)
+            const result = await productCollection.findOne(query)
             res.send(result);
         })
         app.get('/users/:id', async (req, res) => {
